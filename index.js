6a4d5377e944c067d6164f64bf11fa77cfee119e
Aaron Gonzalez <aarong.1503@gmail.com>
2020-12-20 20:45:48 -0500

    Share button function added

---
+++ index.js
@@ -0,0 +1,10 @@
+let $share = document.getElementById("share");
+let $overcontain = document.getElementById("overContain");
+
+function show() {
+  $overcontain.style.visibility = "visible";
+}
+
+function hide() {
+  $overcontain.style.visibility = "hidden";
+}
